f39e1e0f1a97fc305e9ffaa81556301baa4f7d01
trinketbot <carolinewilkinson96@gmail.com>
2026-02-22 00:00:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -377,12 +377,12 @@ async function handleInteraction(d) {
             inline: false,
           },
           {
-            name: "**Price Caps**\nCurrent Releases",
+            name: "Current Release Caps",
             value: "**Standard Items**\n+$10 if $1-$49 Retail\n+$15 if $50-$99 Retail\n+$20 if $100+ Retail\n\n**Secrets**\n*Market rate*\n\n**Exclusives**\n*Market rate*",
             inline: true,
           },
           {
-            name: " \n \nRetired Items",
+            name: "Retired Item Caps",
             value: "**Standard Items**\n+$20 if $1-$49 Retail\n+$30 if $50-$99 Retail\n+$40 if $100+ Retail\n\n**Secrets**\n*Market rate*\n\n**Exclusives**\n*Market rate*",
             inline: true,
           },
